179473cc6a110f7355f012df8d2dd89bb374658c
Mazharul <mazharul.jatri@gmail.com>
2025-01-22 17:41:33 +0600

    custom validation

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -3,6 +3,7 @@ import {TasksService} from "./tasks.service";
 import { Task, TaskStatus } from "./task.model";
 import { CreateTaskDto } from "./dto/create-task.dto";
 import { GetTasksFilterDto } from "./dto/get-tasks-filter.dto";
+import { TaskStatusValidationPipe } from "./pipes/task-status-validation.pipe";
 
 @Controller('tasks')
 export class TasksController {
@@ -29,7 +30,9 @@ export class TasksController {
   }
 
   @Patch('/:id/status')
-  updateTask(@Param('id') id: string, @Body('status') status: TaskStatus)
+  updateTask(
+    @Param('id') id: string,
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus)
   {
     return this.taskService.updateTask(id, status)
   }
